f2847a158f7ab8a728cb59015932aa153979b632
Heliconia <macromicroffl@outlook.com>
2024-06-25 11:55:42 +0300

    asdasda

--- SmartWorkout/Components/Pages/Counter.razor.cs
+++ SmartWorkout/Components/Pages/Counter.razor.cs
@@ -1,9 +1,19 @@
-﻿namespace SmartWorkout.Components.Pages
+﻿
+namespace SmartWorkout.Components.Pages
 {
+
     public partial class Counter
     {
         private int currentCount = 0;
 
+        bool visible = true;
+
+        Task OnClicked()
+        {
+            visible = false;
+
+            return Task.CompletedTask;
+        }
         private void IncrementCount()
         {
             currentCount++;
